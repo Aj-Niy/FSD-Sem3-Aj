7faa9346e872d4854eb996ea10be39573c658299
Ajit Pandey <ajitpandey4572@gmail.com>
2026-08-21 14:33:07 +0530

    initialize LAB3 with http server and package configuration

---
+++ backend/LAB3/httpServer.js
@@ -0,0 +1,10 @@
+import http from 'http';
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+const port = 3000;
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`); 
+});
