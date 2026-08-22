3ff48c9e2a40dd642a7a31b2df8373fa4d7c47ba
Ajit Pandey <ajitpandey4572@gmail.com>
2026-08-22 17:08:30 +0530

    refactor http server to handle specific GET request for /msg

--- backend/LAB3/httpServer.js
+++ backend/LAB3/httpServer.js
@@ -1,10 +1,15 @@
 import http from 'http';
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    const url=req.url;
+    const method=req.method;
+    if(url=="/msg" && method=="GET"){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World Successful\n');
+    }
 });
 
 const port = 3000;
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}/`); 
 });
+
